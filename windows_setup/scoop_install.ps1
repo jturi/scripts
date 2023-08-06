8125cf4c092964d33aa61d50d7594c5a25843290
jturi <josephturi@gmail.com>
2023-08-06 17:30:10 +0200

    Update scoop_install.ps1

--- windows_setup/scoop_install.ps1
+++ windows_setup/scoop_install.ps1
@@ -1,28 +1,7 @@
-# https://github.com/microsoft/terminal$string = 'something'
-# https://scoop.sh/
+# Scoop. A command-line installer for Windows
+# Scoop installs programs you know and love, from the command line with a minimal amount of friction.
+# Website: https://scoop.sh/
 
-# Install scoop if not installed
-try { scoop info }
-catch { 
-    "Scoop is not installed."
-    "Intalling scoop now."
-    Set-ExecutionPolicy RemoteSigned -scope CurrentUser
-    Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
-    }
-# Install applications
-finally {
-    $string = $(scoop info)
-    # [bool]$string
-    if ($string) {
-        "Scoop installed."
-        "Installing appications with scoop..."
-        # Install git
-        scoop install git
-        # Add bucket for more packages
-        scoop bucket add extras
-        # Install packages
-        scoop install 7zip Audacity autohotkey brave calibre conemu copyq discord ditto firefox cmder foxit-reader curl git googlechrome greenshot obs-studio irfanview   make meld openhardwaremonitor putty python qbittorrent screentogif totalcommander vlc vscode winscp
-        # List installed packages
-        scoop list
-        }
-}
+# Open Powershell X86 (Do not use run As Administrator, not needed)
+# Run the following one-liner command:
+Set-ExecutionPolicy RemoteSigned -scope CurrentUser; Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh'); scoop install git; scoop bucket add extras; scoop install 7zip Audacity autohotkey brave calibre conemu copyq discord ditto firefox cmder foxit-reader curl git googlechrome greenshot obs-studio irfanview   make meld openhardwaremonitor putty python qbittorrent screentogif totalcommander vlc vscode winscp
